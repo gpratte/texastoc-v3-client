57c6dfe808e607dbd8bdfca3305cf1d3bcd05d95
Gil Pratte <gil.pratte@gmail.com>
2021-05-16 10:08:59 -0500

    version 3.10 clock polling

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,17 +1,22 @@
 
-export const VERSION = "3.00";
+export const VERSION = "3.10";
 
 // This UI Application (React/Redux)
 // Local non-tomcat
 //export const CLIENT_URL = "http://localhost:3000";
+
 // Local tomcat
 //export const CLIENT_URL = "http://localhost:8080";
+
 // Local webapp-runner
 //export const CLIENT_URL = "http://localhost:9090";
+
 // Heroku
 export const CLIENT_URL = "https://texastoc.herokuapp.com";
+
 // Production texastoc
 //export const CLIENT_URL = "https://texastoc.com";
+
 // Production davebpoker - remember to update pom.xml
 //export const CLIENT_URL = "https://davebpoker.com";
 
@@ -19,13 +24,18 @@ export const CLIENT_URL = "https://texastoc.herokuapp.com";
 // Where to find the Server Application
 // Local non-tomcat
 //export const SERVER_URL = "http://localhost:8080";
+
 // Local tomcat
 //export const SERVER_URL = "http://localhost:8080/server";
+
 // Local webapp-runner
 //export const SERVER_URL = "http://localhost:8080";
+
 // Heroku
 export const SERVER_URL = "https://texastoc-server.herokuapp.com";
+
 // Production texastoc
 //export const SERVER_URL = "https://texastoc.com/server";
+
 // Production davebpoker - remember to update pom.xml
 //export const SERVER_URL = "https://davebpoker.com/server";
